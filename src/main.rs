62a6d1669bbbc402ba82d7aa1609c08d2513dedf
Jude Muriithi <muriithi@princeton.edu>
2023-06-27 03:42:46 -0700

    Whole project initial commit

--- src/main.rs
+++ src/main.rs
@@ -2,64 +2,58 @@ use std::time::Instant;
 
 use raylib::prelude::*;
 
-use grid::{Grid, GameState};
-
 const TICKS_PER_SECOND: u128 = 10;
 const NANOS_PER_TICK: u128 = 1_000_000_000 / TICKS_PER_SECOND;
 
-const GRID_SCALE: i32 = 40;
-const GRID_WIDTH: i32 = 20;
-const GRID_HEIGHT: i32 = 15;
-const U_GRID_WIDTH: usize = GRID_WIDTH as usize;
-const U_GRID_HEIGHT: usize = GRID_HEIGHT as usize;
+const GRID_SCALE: i32 = 20;
+const GRID_WIDTH: i32 = 40;
+const GRID_HEIGHT: i32 = 30;
 
 const SCREEN_WIDTH: i32 = GRID_WIDTH * GRID_SCALE;
 const SCREEN_HEIGHT: i32 = GRID_HEIGHT * GRID_SCALE;
+const U_GRID_WIDTH: usize = GRID_WIDTH as usize;
+const U_GRID_HEIGHT: usize = GRID_HEIGHT as usize;
 
 const GRID_COLOR: Color = Color::LIGHTGRAY;
 const LIVE_COLOR: Color = Color::BLACK;
 const DEAD_COLOR: Color = Color::WHITE;
 
-/// Transforms vector pixel coordinates into indices on the automata grid.
-fn px_to_square(Vector2 { x, y }: Vector2) -> (i32, i32) {
-    let x = (x as i32).clamp(0, SCREEN_WIDTH - GRID_SCALE);
-    let y = (y as i32).clamp(0, SCREEN_HEIGHT - GRID_SCALE);
-    let x = x - (x % GRID_SCALE);
-    let y = y - (y % GRID_SCALE);
-
-    let x = x / GRID_SCALE;
-    let y = y / GRID_SCALE;
-    (x, y)
+enum GameState {
+    EDITING,
+    RUNNING,
 }
 
 fn main() {
     let (mut rl, thread) =
-        raylib::init().size(SCREEN_WIDTH, SCREEN_HEIGHT).build();
+        raylib::init().size(SCREEN_WIDTH, SCREEN_HEIGHT).vsync().build();
 
     let start = Instant::now();
     let mut current_tick: u128 = 0;
 
-    let mut grid = Grid::new();
+    let mut grid = new_grid();
+    let mut num_alive = 0;
 
-    let mut state = GameState::CREATING;
+    let mut state = GameState::EDITING;
 
     while !rl.window_should_close() {
         match state {
-            GameState::CREATING => {
+            GameState::EDITING => {
                 // Use mouse to set squares
                 if rl.is_mouse_button_down(MouseButton::MOUSE_LEFT_BUTTON) {
                     let (x, y) = px_to_square(rl.get_mouse_position());
-                    grid.set_true(x as usize, y as usize);
-                    println!("Neighbors: {}", grid.live_neighbors(x as usize, y as usize))
+                    grid[y][x] = true;
+                    num_alive += 1;
                 }
                 if rl.is_mouse_button_down(MouseButton::MOUSE_RIGHT_BUTTON) {
                     let (x, y) = px_to_square(rl.get_mouse_position());
-                    grid.set_false(x as usize, y as usize);
+                    grid[y][x] = false;
+                    num_alive -= 1;
                 }
 
-                // Start automata with space key
-                if let Some(KeyboardKey::KEY_SPACE) = rl.get_key_pressed() {
-                    state = GameState::RUNNING;
+                match rl.get_key_pressed() {
+                    Some(KeyboardKey::KEY_SPACE) => state = GameState::RUNNING,
+                    Some(KeyboardKey::KEY_X) => grid = new_grid(),
+                    _ => ()
                 }
             }
             GameState::RUNNING => {
@@ -68,35 +62,36 @@ fn main() {
                 if tick > current_tick {
                     current_tick = tick;
 
-                    let mut next = Grid::new();
+                    let mut next_grid = new_grid();
+                    let mut next_num_alive = 0;
 
-                    for x in 0..GRID_WIDTH {
-                        for y in 0..GRID_HEIGHT {
-                            let x = x as usize;
-                            let y = y as usize;
+                    for x in 0..U_GRID_WIDTH {
+                        for y in 0..U_GRID_HEIGHT {
+                            let neighbor_count = living_neighbors(&grid, x, y);
 
-                            let neighbor_count = grid.live_neighbors(x, y);
-
-                            if grid.is_alive(x, y) {
+                            if grid[y][x] {
                                 if neighbor_count == 2 || neighbor_count == 3 {
-                                    next.set_true(x, y);
+                                    next_grid[y][x] = true;
+                                    next_num_alive += 1;
                                 }
                             } else {
                                 if neighbor_count == 3 {
-                                    next.set_true(x, y);
+                                    next_grid[y][x] = true;
+                                    next_num_alive += 1;
                                 }
                             }
                         }
                     }
 
-                    grid = next;
+                    grid = next_grid;
+                    num_alive = next_num_alive;
                 }
 
-                if grid.num_alive == 0 {
-                    state = GameState::CREATING;
+                if num_alive == 0 {
+                    state = GameState::EDITING;
                 }
                 if let Some(KeyboardKey::KEY_SPACE) = rl.get_key_pressed() {
-                    state = GameState::CREATING;
+                    state = GameState::EDITING;
                 }
             }
         }
@@ -106,7 +101,7 @@ fn main() {
         // Squares
         for x in 0..GRID_WIDTH {
             for y in 0..GRID_HEIGHT {
-                if grid.is_alive(x as usize, y as usize) {
+                if grid[y as usize][x as usize] {
                     d.draw_rectangle(
                         x * GRID_SCALE,
                         y * GRID_SCALE,
@@ -137,73 +132,45 @@ fn main() {
         }
 
         // Create mode tag
-        if let GameState::CREATING = state {
-            d.draw_text("Create Mode", 5, 5, 20, Color::GOLD);
+        if let GameState::EDITING = state {
+            d.draw_text("Edit Mode", 15, 15, 25, Color::DARKBLUE);
         }
     }
 }
 
-mod grid {
-    use crate::{GRID_HEIGHT as GH, GRID_WIDTH as GW};
+/// Transforms vector pixel coordinates into indices on the automata grid.
+fn px_to_square(Vector2 { x, y }: Vector2) -> (usize, usize) {
+    let x = (x as i32).clamp(0, SCREEN_WIDTH - GRID_SCALE);
+    let y = (y as i32).clamp(0, SCREEN_HEIGHT - GRID_SCALE);
+    let x = x - (x % GRID_SCALE);
+    let y = y - (y % GRID_SCALE);
 
-    const GRID_WIDTH: usize = GW as usize;
-    const GRID_HEIGHT: usize = GH as usize;
+    let x = x / GRID_SCALE;
+    let y = y / GRID_SCALE;
+    (x as usize, y as usize)
+}
 
-    pub enum GameState {
-        CREATING,
-        RUNNING,
-    }
+fn new_grid() -> Vec<Vec<bool>> {
+    vec![vec![false; U_GRID_WIDTH]; U_GRID_HEIGHT]
+}
 
-    pub struct Grid {
-        squares: Vec<Vec<bool>>,
-        pub num_alive: u128,
-    }
+pub fn living_neighbors(grid: &Vec<Vec<bool>>, x: usize, y: usize) -> u8 {
+    let left = if x == 0 { x } else { x - 1 };
+    let right = if x == U_GRID_WIDTH - 1 { x } else { x + 1 };
+    let above = if y == 0 { y } else { y - 1 };
+    let below = if y == U_GRID_HEIGHT - 1 { y } else { y + 1 };
 
-    impl Grid {
-        pub fn new() -> Self {
-            Grid {
-                squares: vec![vec![false; GRID_WIDTH]; GRID_HEIGHT],
-                num_alive: 0,
-            }
-        }
-
-        pub fn set_true(&mut self, x: usize, y: usize) {
-            if !self.squares[y][x] {
-                self.squares[y][x] = true;
-                self.num_alive += 1;
-            }
-        }
-
-        pub fn set_false(&mut self, x: usize, y: usize) {
-            if self.squares[y][x] {
-                self.squares[y][x] = false;
-                self.num_alive -= 1;
-            }
-        }
-
-        pub fn is_alive(&self, x: usize, y: usize) -> bool {
-            self.squares[y][x]
-        }
-
-        pub fn live_neighbors(&self, x: usize, y: usize) -> u8 {
-            let left = if x == 0 { x } else { x - 1 };
-            let right = if x == GRID_WIDTH - 1 { x } else { x + 1 };
-            let above = if y == 0 { y } else { y - 1 };
-            let below = if y == GRID_HEIGHT - 1 { y } else { y + 1 };
-
-            [
-                self.squares[y][left],
-                self.squares[y][right],
-                self.squares[above][x],
-                self.squares[below][x],
-                self.squares[above][left],
-                self.squares[above][right],
-                self.squares[below][left],
-                self.squares[below][right],
-            ]
-            .into_iter()
-            .map(|b| b as u8)
-            .sum()
-        }
-    }
+    [
+        grid[y][left],
+        grid[y][right],
+        grid[above][x],
+        grid[below][x],
+        grid[above][left],
+        grid[above][right],
+        grid[below][left],
+        grid[below][right],
+    ]
+    .into_iter()
+    .map(|b| b as u8)
+    .sum()
 }
