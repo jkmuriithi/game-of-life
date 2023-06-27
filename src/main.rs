97042d3e16ce2ff7daafda34a1e4ae436d2fe520
Jude Muriithi <muriithi@princeton.edu>
2023-06-27 04:17:14 -0700

    Fix still_frame bug and add docs

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,40 @@
+//! # Game of Life
+//!
+//! A simple simulation of Conway's Game of Life in
+//! [raylib-rs](https://github.com/deltaphc/raylib-rs).
+//!
+//! Controls:
+//! - *Edit Mode:* Left-click to create a tile. Right-click to remove a tile.
+//! `x` to clear all tiles. `SPACE` to enter *Automata Mode*.
+//! - *Automata Mode:* `SPACE` to re-enter *Edit Mode*. The program should also
+//! automatically return to *Edit Mode* when there are either no living elements
+//! or no moving elements on screen.
+//!
+//! **Author:** Jude Muriithi (GitHub:
+//! [jkmuriithi](https://github.com/jkmuriithi))
+//!
+
 use std::time::Instant;
 
+use raylib::consts::MouseButton::*;
 use raylib::prelude::*;
 
+// Game configuration constants
 const TICKS_PER_SECOND: u128 = 10;
-const NANOS_PER_TICK: u128 = 1_000_000_000 / TICKS_PER_SECOND;
-
 const GRID_SCALE: i32 = 20;
 const GRID_WIDTH: i32 = 40;
 const GRID_HEIGHT: i32 = 30;
 
+const GRID_COLOR: Color = Color::LIGHTGRAY;
+const LIVE_COLOR: Color = Color::BLACK;
+const DEAD_COLOR: Color = Color::WHITE;
+
+// Helper constants
+const NANOS_PER_TICK: u128 = 1_000_000_000 / TICKS_PER_SECOND;
 const SCREEN_WIDTH: i32 = GRID_WIDTH * GRID_SCALE;
 const SCREEN_HEIGHT: i32 = GRID_HEIGHT * GRID_SCALE;
 const U_GRID_WIDTH: usize = GRID_WIDTH as usize;
 const U_GRID_HEIGHT: usize = GRID_HEIGHT as usize;
-
-const GRID_COLOR: Color = Color::LIGHTGRAY;
-const LIVE_COLOR: Color = Color::BLACK;
-const DEAD_COLOR: Color = Color::WHITE;
 
 enum GameState {
     EDITING,
@@ -24,26 +42,22 @@ enum GameState {
 }
 
 fn main() {
-    let (mut rl, thread) = raylib::init()
-        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
-        .vsync()
-        .build();
+    let (mut rl, thread) =
+        raylib::init().size(SCREEN_WIDTH, SCREEN_HEIGHT).vsync().build();
 
     let start = Instant::now();
     let mut current_tick: u128 = 0;
-
-    let mut grid = new_grid();
     let mut state = GameState::EDITING;
 
+    let mut grid = new_grid();
     while !rl.window_should_close() {
         match state {
             GameState::EDITING => {
                 // Use mouse to set squares
-                if rl.is_mouse_button_down(MouseButton::MOUSE_LEFT_BUTTON) {
+                if rl.is_mouse_button_down(MOUSE_LEFT_BUTTON) {
                     let (x, y) = px_to_square(rl.get_mouse_position());
                     grid[y][x] = true;
-                }
-                if rl.is_mouse_button_down(MouseButton::MOUSE_RIGHT_BUTTON) {
+                } else if rl.is_mouse_button_down(MOUSE_RIGHT_BUTTON) {
                     let (x, y) = px_to_square(rl.get_mouse_position());
                     grid[y][x] = false;
                 }
@@ -72,8 +86,7 @@ fn main() {
                                 if neighbor_count == 2 || neighbor_count == 3 {
                                     next_grid[y][x] = true;
                                     num_alive += 1;
-                                }
-                                else {
+                                } else {
                                     still_frame = false;
                                 }
                             } else {
@@ -86,7 +99,7 @@ fn main() {
                         }
                     }
 
-                    if still_frame || num_alive == 0  {
+                    if still_frame || num_alive == 0 {
                         state = GameState::EDITING;
                     }
 
@@ -101,7 +114,7 @@ fn main() {
 
         let mut d = rl.begin_drawing(&thread);
 
-        // Squares
+        // Draw squares
         for x in 0..GRID_WIDTH {
             for y in 0..GRID_HEIGHT {
                 if grid[y as usize][x as usize] {
@@ -124,7 +137,7 @@ fn main() {
             }
         }
 
-        // Grid lines
+        // Draw grid lines
         for i in 1..GRID_WIDTH {
             let x_px = i * GRID_SCALE;
             d.draw_line(x_px, 0, x_px, SCREEN_HEIGHT, GRID_COLOR);
@@ -134,7 +147,6 @@ fn main() {
             d.draw_line(0, y_px, SCREEN_WIDTH, y_px, GRID_COLOR);
         }
 
-        // Create mode tag
         if let GameState::EDITING = state {
             d.draw_text("Edit Mode", 15, 15, 25, Color::DARKBLUE);
         }
@@ -153,11 +165,13 @@ fn px_to_square(Vector2 { x, y }: Vector2) -> (usize, usize) {
     (x as usize, y as usize)
 }
 
+/// Returns a new automata grid filled with dead squares.
 fn new_grid() -> Vec<Vec<bool>> {
     vec![vec![false; U_GRID_WIDTH]; U_GRID_HEIGHT]
 }
 
-pub fn living_neighbors(grid: &Vec<Vec<bool>>, x: usize, y: usize) -> u8 {
+/// Returns the number of living neighbors which square `(x, y)` has in `grid`.
+fn living_neighbors(grid: &Vec<Vec<bool>>, x: usize, y: usize) -> u8 {
     let left = if x == 0 { x } else { x - 1 };
     let right = if x == U_GRID_WIDTH - 1 { x } else { x + 1 };
     let above = if y == 0 { y } else { y - 1 };
